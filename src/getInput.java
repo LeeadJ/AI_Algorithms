e4295b2ce8633d68d267f7efe075a7086e660175
LeeadJ <leead123@gmail.com>
2022-11-24 14:58:34 +0200

    getInput - extractfile

--- src/getInput.java
+++ src/getInput.java
@@ -14,4 +14,20 @@ public class getInput {
         this._filePath = input;
         this._container = new ArrayList<>();
     }
+
+    /** This function extracts the queries from the input file and saves the xml path for the xml reader.*/
+    public void extractFile() {
+        try{
+            FileReader fr = new FileReader(this._filePath);
+            BufferedReader br = new BufferedReader(fr);
+            this._xmlPath = br.readLine();
+            String str = br.readLine();
+            while(str != null) {
+                this._container.add(str);
+                str = br.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
