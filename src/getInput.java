f58f48c0c3d03f31b549caab2f2aeca087ae59fe
LeeadJ <leead123@gmail.com>
2022-11-24 14:58:03 +0200

    getInput - constructor

--- src/getInput.java
+++ src/getInput.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 public class getInput {
+    private String _filePath;
+    public ArrayList<String> _container;
+    public String _xmlPath;
+
+    /**Constructor for getInput
+     * @param input - path of txt file.*/
+    public getInput(String input){
+        this._filePath = input;
+        this._container = new ArrayList<>();
+    }
 }
