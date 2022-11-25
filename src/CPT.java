7b85f04689aaaaab12d25d9963d6cd5485ca5d08
LeeadJ <leead123@gmail.com>
2022-11-25 14:50:31 +0200

    update 25/11/22 14:31

--- src/CPT.java
+++ src/CPT.java
@@ -29,9 +29,4 @@ public class CPT {
             cpt += map.toString() + "\n";
         return cpt;
     }
-
-
-
-
-
 }
--- src/Ex1.java
+++ src/Ex1.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Ex1 {
     public static void main(String[] args) {
 
-        xml_Parser xml = new xml_Parser("alarm_net.xml");
+//        xml_Parser xml = new xml_Parser("alarm_net.xml");
+        xml_Parser xml = new xml_Parser("big_net.xml");
         ArrayList<Variable> arr = xml.parse_file();
         for(Variable var : arr)
             System.out.println(var.toString());
