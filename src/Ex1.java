e7b9ef0781698cf3fbb7b0344213d108b052ce71
LeeadJ <leead123@gmail.com>
2022-11-25 14:31:22 +0200

    update 25/11/22 14:31

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,2 +1,11 @@
+import java.io.*;
+import java.util.ArrayList;
 public class Ex1 {
+    public static void main(String[] args) {
+
+        xml_Parser xml = new xml_Parser("alarm_net.xml");
+        ArrayList<Variable> arr = xml.parse_file();
+        for(Variable var : arr)
+            System.out.println(var.toString());
+    }
 }
