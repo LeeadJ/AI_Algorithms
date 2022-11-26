63672a3af10bbeacb4a0d5fa6647c71964696cdb
LeeadJ <leead123@gmail.com>
2022-11-26 23:01:02 +0200

    added functions

---
+++ src/CreateOutput.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CreateOutput {
+    private static ArrayList<String> _lines;
+
+    public static void addLine(String line){
+        if(line == null)
+            _lines = new ArrayList<>();
+        _lines.add(line);
+    }
+
+    public static void writeToFile(){
+        try{
+            FileWriter myWriter = new FileWriter("output.txt");
+            for(String toWrite : _lines){
+                myWriter.write(toWrite);
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error writing file to: output.txt");
+            e.printStackTrace();
+        }
+    }
+}
